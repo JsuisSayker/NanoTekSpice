18151cbcb211cc2aaa4a77757548b9916f911aeb
arthurgauffre <arth.gauffre@gmail.com>
2024-03-01 22:04:25 +0100

    [UPT] fixing Gate4001

--- include/Components/GatesComponents/Gate4001Component.hpp
+++ include/Components/GatesComponents/Gate4001Component.hpp
@@ -17,6 +17,10 @@ namespace nts
         public:
             Gate4001Component();
             nts::Tristate compute(std::size_t pin); 
+            void setLink(std::size_t pin, nts::IComponent &other, std::size_t otherPin);
+            nts::Tristate ThruthTable(nts::Tristate a);
+        protected:
+            std::vector<std::unique_ptr<nts::IComponent>> orComponentsList;
     };
 }
 
--- src/Components/GatesComponents/Gate4001Component.cpp
+++ src/Components/GatesComponents/Gate4001Component.cpp
@@ -14,49 +14,92 @@
 nts::Gate4001Component::Gate4001Component()
 {
     ComponentFactory cf;
-    std::unique_ptr<nts::IComponent> or1 = cf.createOrComponent();
-    std::unique_ptr<nts::IComponent> or2 = cf.createOrComponent();
-    std::unique_ptr<nts::IComponent> or3 = cf.createOrComponent();
-    std::unique_ptr<nts::IComponent> or4 = cf.createOrComponent();
 
-    std::unique_ptr<nts::IComponent> not1 = cf.createNotComponent();
-    std::unique_ptr<nts::IComponent> not2 = cf.createNotComponent();
-    std::unique_ptr<nts::IComponent> not3 = cf.createNotComponent();
-    std::unique_ptr<nts::IComponent> not4 = cf.createNotComponent();
+    this->orComponentsList.push_back(cf.createOrComponent());
+    this->orComponentsList.push_back(cf.createOrComponent());
+    this->orComponentsList.push_back(cf.createOrComponent());
+    this->orComponentsList.push_back(cf.createOrComponent());
 
-    not1->setLink(1, *or1.get(), 3);
-    not2->setLink(1, *or2.get(), 3);
-    not3->setLink(1, *or3.get(), 3);
-    not4->setLink(1, *or4.get(), 3);
-    
-    this->link.push_back(std::make_pair(or1.get(), 1));
-    this->link.push_back(std::make_pair(or1.get(), 2));
-    this->link.push_back(std::make_pair(not1.get(), 1));
-    this->link.push_back(std::make_pair(not2.get(), 1));
-    this->link.push_back(std::make_pair(or2.get(), 2));
-    this->link.push_back(std::make_pair(or2.get(), 1));
+    this->link.push_back(std::make_pair(this->orComponentsList[0].get(), 1));
+    this->link.push_back(std::make_pair(this->orComponentsList[0].get(), 2));
+    this->link.push_back(std::make_pair(this->orComponentsList[0].get(), 3));
+    this->link.push_back(std::make_pair(this->orComponentsList[1].get(), 3));
+    this->link.push_back(std::make_pair(this->orComponentsList[1].get(), 2));
+    this->link.push_back(std::make_pair(this->orComponentsList[1].get(), 1));
     this->link.push_back(std::make_pair(nullptr, 0));
-    this->link.push_back(std::make_pair(or3.get(), 1));
-    this->link.push_back(std::make_pair(or3.get(), 2));
-    this->link.push_back(std::make_pair(not3.get(), 1));
-    this->link.push_back(std::make_pair(not4.get(), 1));
-    this->link.push_back(std::make_pair(or4.get(), 2));
-    this->link.push_back(std::make_pair(or4.get(), 1));
+    this->link.push_back(std::make_pair(this->orComponentsList[2].get(), 1));
+    this->link.push_back(std::make_pair(this->orComponentsList[2].get(), 2));
+    this->link.push_back(std::make_pair(this->orComponentsList[2].get(), 3));
+    this->link.push_back(std::make_pair(this->orComponentsList[3].get(), 3));
+    this->link.push_back(std::make_pair(this->orComponentsList[3].get(), 2));
+    this->link.push_back(std::make_pair(this->orComponentsList[3].get(), 1));
     this->link.push_back(std::make_pair(nullptr, 0));
+}
+
+nts::Tristate nts::Gate4001Component::ThruthTable(nts::Tristate a)
+{
+    if (a == nts::Tristate::True)
+        return nts::Tristate::False;
+    if (a == nts::Tristate::False)
+        return nts::Tristate::True;
+    return nts::Tristate::Undefined;
 }
 
 nts::Tristate nts::Gate4001Component::compute(std::size_t pin)
 {
     if (pin > this->link.size())
         throw "Pin does not exist";
-    printf("\npin: %ld\n", pin);
-    if (dynamic_cast<nts::NotComponent *>(this->link[pin - 1].first))
-        printf("NotComponent\n");
-    if (dynamic_cast<nts::OrComponent *>(this->link[pin - 1].first))
-        printf("OrComponent\n");
-    if (dynamic_cast<nts::Input *>(this->link[pin - 1].first))
-        printf("Input\n");
-    if (dynamic_cast<nts::Output *>(this->link[pin - 1].first))
-        printf("Output\n");
-    return this->link[pin - 1].first->compute(this->link[pin - 1].second);
+    if (pin == 3)
+        return ThruthTable(link[2].first->compute(link[2].second));
+    if (pin == 4)
+        return ThruthTable(link[3].first->compute(link[3].second));
+    if (pin == 10)
+        return ThruthTable(link[9].first->compute(link[9].second));
+    if (pin == 11)
+        return ThruthTable(link[10].first->compute(link[10].second));
+    return link[pin - 1].first->compute(link[pin - 1].second);
+}
+
+void nts::Gate4001Component::setLink(std::size_t pin, nts::IComponent &other, std::size_t otherPin)
+{
+    switch (pin) {
+        case 1:
+            orComponentsList[0]->setLink(1, other, otherPin);
+            break;
+        case 2:
+            orComponentsList[0]->setLink(2, other, otherPin);
+            break;
+        case 3:
+            orComponentsList[0]->setLink(3, other, otherPin);
+            break;
+        case 4:
+            orComponentsList[1]->setLink(3, other, otherPin);
+            break;
+        case 5:
+            orComponentsList[1]->setLink(2, other, otherPin);
+            break;
+        case 6:
+            orComponentsList[1]->setLink(1, other, otherPin);
+            break;
+        case 8:
+            orComponentsList[2]->setLink(1, other, otherPin);
+            break;
+        case 9:
+            orComponentsList[2]->setLink(2, other, otherPin);
+            break;
+        case 10:
+            orComponentsList[2]->setLink(3, other, otherPin);
+            break;
+        case 11:
+            orComponentsList[3]->setLink(3, other, otherPin);
+            break;
+        case 12:
+            orComponentsList[3]->setLink(2, other, otherPin);
+            break;
+        case 13:
+            orComponentsList[3]->setLink(1, other, otherPin);
+            break;
+        default:
+            break;
+    }
 }
